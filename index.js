53dea16cb0a57d3b50fe40d34f77e1edaef5cbe8
Haoran Feng <haoran.feng@alo7.com>
2017-03-26 10:15:39 +0800

    use pinyinlite to replace pinyin

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var pinyin = require('pinyin')
-let a_name = "周爱请"
-let b_name = "的人"
+var pinyinlite = require('pinyinlite')
+let a_name = "艾老师"
+let b_name = "周老师"
 
 let aNamePinyin = pinyin(a_name, { style: pinyin.STYLE_NORMAL }).map( arr => arr[0] ).join('_')
 let bNamePinyin = pinyin(b_name, { style: pinyin.STYLE_NORMAL }).map( arr => arr[0] ).join('_')
 console.log(aNamePinyin.localeCompare(bNamePinyin));
+
+let aNamePinyinlite = pinyinlite(a_name).map( arr => arr[0]).join('_')
+let bNamePinyinlite = pinyinlite(b_name).map( arr => arr[0]).join('_')
+console.log(aNamePinyinlite)
+console.log(bNamePinyinlite)
+console.log(aNamePinyinlite.localeCompare(bNamePinyin));
