1b0c853ee722f31fc08f7419badb20435ca6491a
Haoran Feng <haoran.feng@alo7.com>
2017-03-26 10:08:05 +0800

    bigger result means show later

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hello world');
+var pinyin = require('pinyin')
+let a_name = "周爱请"
+let b_name = "的人"
+
+let aNamePinyin = pinyin(a_name, { style: pinyin.STYLE_NORMAL }).map( arr => arr[0] ).join('_')
+let bNamePinyin = pinyin(b_name, { style: pinyin.STYLE_NORMAL }).map( arr => arr[0] ).join('_')
+console.log(aNamePinyin.localeCompare(bNamePinyin));
